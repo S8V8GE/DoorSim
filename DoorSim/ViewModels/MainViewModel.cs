69aa2bc68f36bd8b5876077ee5a2ea6b03b69e18
James Savage <jdsavage1985@gmail.com>
2026-04-27 16:00:40 +0100

    Add project files.

---
+++ DoorSim/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DoorSim.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string statusText = "Softwire is not connected";
+}
