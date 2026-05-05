186d2cae9da1b406ccae47cbecbae0ae113d47be
James Savage <jdsavage1985@gmail.com>
2026-05-05 10:26:44 +0100

    Polished two door layout

---
+++ DoorSim/Views/DoorInterlockingControlsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoorSim.Views
+{
+    /// <summary>
+    /// Interaction logic for DoorInterlockingControlsView.xaml
+    /// </summary>
+    public partial class DoorInterlockingControlsView : UserControl
+    {
+        public DoorInterlockingControlsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
